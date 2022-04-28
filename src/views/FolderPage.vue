9f0a771352882dc23352b8af135d2aacb087b9fd
Maël Primet <mael.primet@gmail.com>
2022-04-28 16:23:56 +0200

    feat: add tooltip

--- src/views/FolderPage.vue
+++ src/views/FolderPage.vue
@@ -8,28 +8,47 @@
         <ion-title>{{ $route.params.id }}</ion-title>
       </ion-toolbar>
     </ion-header>
-    
+
     <ion-content :fullscreen="true">
       <ion-header collapse="condense">
         <ion-toolbar>
           <ion-title size="large">{{ $route.params.id }}</ion-title>
         </ion-toolbar>
       </ion-header>
-    
+
       <div id="container">
         <strong class="capitalize">{{ $route.params.id }}</strong>
-        <p>Explore <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
+        <div
+          @mouseover="onMouseOver"
+          @mouseleave="onMouseLeave"
+          class="hover-div"
+        >
+          Hover me<br />
+          {{ message }}
+        </div>
       </div>
+      <div ref="tooltipScreen" class="tooltip">Tooltip screen</div>
+      <div ref="tooltipPage" class="tooltip">Tooltip page</div>
+      <div ref="tooltipClient" class="tooltip">Tooltip client</div>
     </ion-content>
   </ion-page>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import { IonButtons, IonContent, IonHeader, IonMenuButton, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
+import { defineComponent } from "vue";
+import {
+  IonButtons,
+  IonContent,
+  IonHeader,
+  IonMenuButton,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+} from "@ionic/vue";
+import { ref, Ref } from "vue";
 
 export default defineComponent({
-  name: 'FolderPage',
+  name: "FolderPage",
   components: {
     IonButtons,
     IonContent,
@@ -37,8 +56,45 @@ export default defineComponent({
     IonMenuButton,
     IonPage,
     IonTitle,
-    IonToolbar
-  }
+    IonToolbar,
+  },
+  setup() {
+    const tooltipScreen: Ref<any> = ref(undefined);
+    const tooltipPage: Ref<any> = ref(undefined);
+    const tooltipClient: Ref<any> = ref(undefined);
+    const message = ref("");
+    function onMouseOver(event: any) {
+      console.log("onMouseOver", event);
+      message.value = `screenX: ${event.screenX}, clientX: ${event.clientX}, pageX: ${event.pageX}`;
+      tooltipScreen.value!.style.top = event.screenY + "px";
+      tooltipScreen.value!.style.left = event.screenX + "px";
+
+      tooltipPage.value!.style.top = event.pageY + "px";
+      tooltipPage.value!.style.left = event.pageX + "px";
+
+      tooltipClient.value!.style.top = event.clientY + "px";
+      tooltipClient.value!.style.left = event.clientX + "px";
+
+      tooltipScreen.value!.style.visibility = "visible";
+      tooltipPage.value!.style.visibility = "visible";
+      tooltipClient.value!.style.visibility = "visible";
+    }
+    function onMouseLeave(event: any) {
+      console.log("onMouseLeave", event);
+      message.value = "";
+      tooltipScreen.value!.style.visibility = "hidden";
+      tooltipPage.value!.style.visibility = "hidden";
+      tooltipClient.value!.style.visibility = "hidden";
+    }
+    return {
+      onMouseOver,
+      onMouseLeave,
+      tooltipScreen,
+      tooltipPage,
+      tooltipClient,
+      message,
+    };
+  },
 });
 </script>
 
@@ -66,5 +122,18 @@ export default defineComponent({
 
 #container a {
   text-decoration: none;
+}
+
+.hover-div {
+  margin: 15px;
+  padding: 15px;
+  background: lightblue;
+}
+
+.tooltip {
+  position: absolute;
+  visibility: hidden;
+  background: white;
+  border: 1px solid #eeeeee;
 }
 </style>
